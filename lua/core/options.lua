18ac107f83004d3de18402b549fc11752cb20d72
mynamelcz <997170576@qq.com>
2022-12-21 23:41:48 +0800

    update options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -28,7 +28,7 @@ opt.backup = false -- 是否开启文件备份
 opt.swapfile = false -- 是否开启交换文件
 opt.writebackup = false
 opt.pumheight = 15 -- 补全最多显示15行
-opt.cmdheight = 0 -- 不输入命令时隐藏命令行
+-- opt.cmdheight = 0 -- 不输入命令时隐藏命令行
 opt.lazyredraw = true -- lazily redraw screen
 
 -- -- jkhl 移动时光标周围保留8行
